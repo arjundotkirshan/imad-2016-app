e3c9c3b2742e8218422253349ade2a7485092c55
arjundotkirshan <arjun.kirshan@gmail.com>
2016-10-30 20:24:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+app.get('/test-db', function (req, res)){
+    
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
