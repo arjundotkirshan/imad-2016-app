a85fa74e78d0c120537a0b1b1ba18e3ad375198a
arjundotkirshan <arjun.kirshan@gmail.com>
2016-10-30 20:39:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,10 +4,26 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
+var Pool = require('pg').Pool;
 
-app.get('/test-db', function (req, res)){
-    
-}
+var config={
+    user:'arjundotkirshan',
+    database:'arjundotkirshan',
+    host:'db.imad.hasura-app.io',
+    port:'5432',
+    password:process.env.DB_PASSWORD
+};
+
+var pool=new Pool(config);
+app.get('/test-db', function (req, res){
+    pool.query('SELECT * FROM test', function(err, result){
+        if(err){
+            res.status(500).send(err.toString());
+        }else{
+            res.send(JSON.stringify(result));
+        }
+    });
+});
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
