82cf4372a625cc3857026555a42f8e0da61656d5
PK Sharma <arjun.kirshan@gmail.com>
2016-11-22 23:20:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,22 @@
-console.log('Loaded!');
+var submit=document.getElementById('submit_btn');
+submit.onclick=function(){
+  var request=new XMLHttpRequest();
+  request.onreadystatechange=function(){
+    if(request.readyState===XMLHttpRequest.DONE){
+        if(request.status===200){
+            alert('Logged in successfully');
+        }else if(request.status===403){
+            alert('Username/password is incorrect');
+        }else if(request.status===500){
+            alert('Something went wrong on the server');
+        }
+    }  
+  };
+  //make the request
+  var username=document.getElementById('username').value;
+  var password=document.getElementById('password').value;
+  console.log(username);
+  console.log(password);
+  request.open('POST', 'http://arjundotkirshan.imad.hasura-app.io/login', true);
+  request.send(json.stringify({username:username, password:password}));
+};
