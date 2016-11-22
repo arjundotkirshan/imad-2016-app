077d995876b351be37939a37f4ab3e9f057c349f
PK Sharma <arjun.kirshan@gmail.com>
2016-11-22 23:41:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -18,5 +18,6 @@ submit.onclick=function(){
   console.log(username);
   console.log(password);
   request.open('POST', 'http://arjundotkirshan.imad.hasura-app.io/login', true);
+  request.setRequestHeader('Content-Type', 'application/json');
   request.send(JSON.stringify({username:username, password:password}));
 };
