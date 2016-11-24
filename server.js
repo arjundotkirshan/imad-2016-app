3eb77e7d911e5efb37af40fdf9d90aaf98706002
PK Sharma <arjun.kirshan@gmail.com>
2016-11-24 09:45:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ function createTemplate (data) {
               ${title}
           </title>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <link href="../ui/style.css" rel="stylesheet" />
+          <link href="/ui/style.css" rel="stylesheet" />
           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
            <link href="../css/bootstrap.min.css" rel="stylesheet">
             <link href="../css/clean-blog.min.css" rel="stylesheet">
